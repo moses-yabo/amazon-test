73a88854060199b1cd13ab0cf9b2630e0763d8ca
Yabo Lindile <yabo.lindile@younglings.africa>
2023-01-20 00:12:13 +0200

    secont

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-  res.send("hello mtsahna ka malume");
+  res.send("hello mtsahna ka malume tristian");
 });
 app.listen(port, () => {
   console.log("Aweya", port);
